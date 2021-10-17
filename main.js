8bbe17470c17ecd3de455f5f59bd96096d6845c1
hadicodess <85850834+hadicodess@users.noreply.github.com>
2021-10-17 12:59:15 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,37 @@
+difference = 0; 
+rightWristX = 0; 
+leftWristX = 0; 
+function setup() 
+{
+    video = createCapture(VIDEO);
+    video.size(550, 500); 
+
+    canvas = createCanvas(550, 500); 
+    canvas.position(560, 150); 
+    poseNet = ml5.poseNet(video, modelLoaded); 
+    poseNet.on('pose', gotPoses); 
+} 
+function draw()
+{
+    background('#2CE863'); 
+    fill('#07F3D1'); 
+    text('PRINCE') 
+    textSize(difference)
+} 
+function modelLoaded()
+{
+    console.log("PoseNet is initialized!");
+} 
+function gotPoses(results)
+{
+if(results.length > 0)
+{
+    console.log(results)
+
+    leftWristX = results[0].pose.leftWrist.x;
+    rightWristX = results[0].pose.rightWrist.x; 
+    difference = floor(leftWristX - rightWristX);  
+
+
+}
+}
